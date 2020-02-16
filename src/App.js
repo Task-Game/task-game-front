f3743f69cf36a39ef05d87cedc2a2118f3755c5b
Raphael <raphael.pieroni@aluno.soueinstein.com.br>
2020-02-16 18:43:21 -0300

    Ajustar tamanhos e cores, definir textos e imagens

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 
-
-
-
 import Menu from './components/menu';
 import Grid from './components/grid';
+import UpGrid from './components/upgrid';
+import Footer from './components/footer';
+
+import './css/mainPage.css';
 
 function App() {
   return (
     <>
     <Menu />
     <Grid />
-  </>
+    <UpGrid />
+    <Footer />
+    </>
 
   );
 }
