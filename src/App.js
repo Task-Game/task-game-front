9326fb2eb293799b0ebfa9e4460c4b665388f78c
Davi3000 <nearwiner1234657@gmail.com>
2020-02-21 21:49:29 -0300

    Arrumar cores

--- src/App.js
+++ src/App.js
@@ -13,6 +13,12 @@ function App() {
     <Menu />
     <Grid />
     <UpGrid />
+    <div class="ui message violet" id="container">
+  <div class="header">Mais sobre o TaskGame</div>
+  <p>
+    Coisas legais sobre o TaskGame
+  </p>
+</div>
     <Footer />
     </>
 
